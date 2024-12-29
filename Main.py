d7d222a9960e2475cd156977d603d6261eb820cf
Нелипович Евгений <zhenya.nelipovich@gmail.com>
2024-12-29 15:50:45 +0300

    Обновление - Добавлено меню - Добавлены комментарии - кнопки 'в меню', когда проиграл - монстры быстрее стали появляться

--- Main.py
+++ Main.py
@@ -2,20 +2,32 @@ from pygame import *
 
 init()
 clock = time.Clock()
-kol_ghost = 0
-heart = 3
 
+
+# Музыка
 music_damage = mixer.Sound("music/fun_damage.mp3")
 music_am = mixer.Sound("music/fun_am.mp3")
 
-screen = display.set_mode((1280, 720))
-display.set_caption("Игра")
-running_display = True
+# Шрифт
 fonts = font.Font('font/ofont.ru_Zeitmax.ttf', 100)
 font_point = font.Font('font/ofont.ru_Etude Noire.ttf', 35)
+
+# Тексты
+display.set_caption("Игра")
 dead_label = fonts.render(f"Вы проиграли!", True, "black")
-back_label = fonts.render("Заново", True, "white")
-game_window = True
+back_label = fonts.render("Заново", True, "red", "black")
+menu_label = fonts.render("В меню", True, "red", "black")
+
+start_label = fonts.render("Играть", True, "black")
+quit_label = fonts.render("Выход", True, "black")
+
+
+# Игра
+screen = display.set_mode((1280, 720))
+game_window = 'main'
+kol_ghost = 0
+heart = 3
+running_display = True
 
 left_move = [
     image.load("img/movement/left_1.png").convert_alpha(),
@@ -59,8 +71,9 @@ list_hill = []
 list_heart = []
 
 # таймер
+millis = 1500
 times = USEREVENT + 1
-time.set_timer(times, 1700)
+time.set_timer(times, millis)
 
 
 while running_display:
@@ -69,11 +82,22 @@ while running_display:
     # Анимация фона
     screen.blit(bg, (bg_x1, 0))
     screen.blit(bg, (bg_x1 + 1280, 0))
+    clock.tick(80)
 
     if bg_x1 == -1280:
         bg_x1 = 0
     bg_x1 -= 5
-    if game_window:
+
+    if game_window == 'main':
+        screen.blit(start_label, (410, 250))
+        screen.blit(quit_label, (430, 400))
+        if start_label.get_rect(topleft=(410, 250)).collidepoint(mouses) and mouse.get_pressed()[0]:
+            game_window = 'game'
+        if quit_label.get_rect(topleft=(430, 400)).collidepoint(mouses) and mouse.get_pressed()[0]:
+            game_window = 'quit'
+        display.update()
+    elif game_window == 'game':
+
         man_rect = right_move[0].get_rect(topleft=(man_x, man_y))
         screen.blit(font_point.render(f"Счёт: {score}", True, "black"), (0, 0))
 
@@ -91,7 +115,7 @@ while running_display:
 
                 if man_rect.colliderect(el):
                     if heart == 1:
-                        game_window = False
+                        game_window = 'fail'
                     else:
                         heart -= 1
                         music_damage.play()
@@ -152,15 +176,14 @@ while running_display:
             frame_counter = 0
 
         display.update()
-        clock.tick(80)
-    else:
+    elif game_window == 'fail':
         screen.fill("red")
-        back_rect = back_label.get_rect(topleft=(430, 600))
-        screen.blit(dead_label, (250, 200))
-        screen.blit(fonts.render(f"Счёт: {score}", True, "white"), (420, 400))
-        screen.blit(back_label, (430, 600))
+        screen.blit(dead_label, (250, 50))
+        screen.blit(fonts.render(f"Счёт: {score}", True, "white"), (420, 200))
+        screen.blit(back_label, (420, 400))
+        screen.blit(menu_label, (430, 550))
         display.update()
-        if back_rect.collidepoint(mouses) and mouse.get_pressed()[0]:
+        if back_label.get_rect(topleft=(420, 400)).collidepoint(mouses) and mouse.get_pressed()[0]:
             list_ghost.clear()
             list_hill.clear()
 
@@ -173,17 +196,32 @@ while running_display:
             man_jump = 10
             is_jump = False
             frame_counter = 0
-            game_window = True
+            game_window = 'game'
+
+        if menu_label.get_rect(topleft=(430, 550)).collidepoint(mouses) and mouse.get_pressed()[0]:
+            list_ghost.clear()
+            list_hill.clear()
+
+            score = 0
+            heart = 3
+            kol_ghost = 0
+            value = 0
+            man_x = 300
+            man_y = 450
+            man_jump = 10
+            is_jump = False
+            frame_counter = 0
+            game_window = 'main'
 
     for e in event.get():
-        if e.type == QUIT:
+        if e.type == QUIT or game_window == 'quit':
             running_display = False
             quit()
         if e.type == times:
 
             kol_ghost += 1
             new_ghost = ghost.get_rect(topleft=(1280, 475))
-            new_ghost.inflate_ip(-20, -20)
+            new_ghost.inflate_ip(-20, -20)  # уменьшение квадрата
             list_ghost.append(new_ghost)
 
             if kol_ghost == 5:
